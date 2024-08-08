390ddd2e2bff8bf83ea8e4230143ef205784486b
usbTypeF <usbtypef@gmail.com>
2024-08-08 03:42:41 -0500

    v3 update

--- cputest.c
+++ cputest.c
@@ -5,20 +5,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
+
 
 #define ARRAY_SIZE 1024 * 32
-#define TEST_DURATION_MS 1000  // 30 seconds test duration
+#define GRAPH_WIDTH 100
+#define GRAPH_HEIGHT 20
+
+int perf = 0;
 
 typedef struct {
     DWORD thread_id;
     __m256i* array;
+    volatile LONG64* loop_counter;
     volatile bool* should_exit;
 } ThreadData;
 
-void SetThreadAffinity(HANDLE thread, int core_id) {
-    DWORD_PTR mask = 1ULL << core_id;
-    SetThreadAffinityMask(thread, mask);
+// New structure to hold group information
+typedef struct {
+    WORD groupId;
+    DWORD processorCount;
+} ProcessorGroupInfo;
+
+// Function to get processor group information
+void GetProcessorGroupInfo(ProcessorGroupInfo** groupInfo, DWORD* groupCount) {
+    *groupCount = GetActiveProcessorGroupCount();
+    *groupInfo = (ProcessorGroupInfo*)malloc(*groupCount * sizeof(ProcessorGroupInfo));
+
+    for (WORD i = 0; i < *groupCount; i++) {
+        (*groupInfo)[i].groupId = i;
+        (*groupInfo)[i].processorCount = GetActiveProcessorCount(i);
+    }
 }
+
+
 
 DWORD WINAPI WorkerThread(LPVOID lpParam) {
     ThreadData* data = (ThreadData*)lpParam;
@@ -211,6 +231,8 @@ DWORD WINAPI WorkerThread(LPVOID lpParam) {
 
             
         }
+
+        ++perf;
     }
     
     
@@ -221,90 +243,201 @@ DWORD WINAPI WorkerThread(LPVOID lpParam) {
     return 0;
 }
 
+// Modified SetThreadAffinity function
+void SetThreadAffinity(HANDLE thread, WORD groupId, DWORD processorIndex) {
+    GROUP_AFFINITY groupAffinity = {0};
+    groupAffinity.Group = groupId;
+    groupAffinity.Mask = 1ULL << processorIndex;
+    SetThreadGroupAffinity(thread, &groupAffinity, NULL);
+}
+
+float log2_float(float x) {
+    return log(x) / log(2);
+}
+
+void printASCIILogPointGraph(float* results, int count) {
+    float min = results[0], max = results[0];
+    for (int i = 0; i < count; i++) {
+        if (results[i] < min) min = results[i];
+        if (results[i] > max) max = results[i];
+    }
+
+    float log_min = log2_float(min);
+    float log_max = log2_float(max);
+
+    // Clear the console
+    #ifdef _WIN32
+    system("cls");
+    #else
+    system("clear");
+    #endif
+
+    printf("MFLOPS Performance Graph (Log2 Scale)\n");
+    printf("Range: %.2f (2^%.2f) - %.2f (2^%.2f) MFLOPS\n", min, log_min, max, log_max);
+
+    char graph[GRAPH_HEIGHT][GRAPH_WIDTH];
+    for (int y = 0; y < GRAPH_HEIGHT; y++) {
+        for (int x = 0; x < GRAPH_WIDTH; x++) {
+            graph[y][x] = ' ';
+        }
+    }
+
+    for (int x = 0; x < count && x < GRAPH_WIDTH; x++) {
+        float log_current = log2_float(results[x]);
+        float normalizedCurrent = (log_current - log_min) / (log_max - log_min);
+        int y = (int)((1 - normalizedCurrent) * (GRAPH_HEIGHT - 1));
+        
+        // Only plot the point, not the line
+        if (y >= 0 && y < GRAPH_HEIGHT) {
+            graph[y][x] = '*';
+        }
+    }
+
+    // Print the graph
+    for (int y = 0; y < GRAPH_HEIGHT; y++) {
+        // Print y-axis labels (in powers of 2)
+        float label_value = pow(2, log_max - y * (log_max - log_min) / (GRAPH_HEIGHT - 1));
+        printf("%6.2f |", label_value);
+        
+        for (int x = 0; x < GRAPH_WIDTH; x++) {
+            printf("%c", graph[y][x]);
+        }
+        printf("\n");
+    }
+
+    // Print x-axis
+    printf("       ");
+    for (int x = 0; x < GRAPH_WIDTH; x++) {
+        printf("-");
+    }
+    printf("\n");
+
+    // Print run numbers
+    printf("       ");
+    for (int x = 0; x < count && x < GRAPH_WIDTH; x++) {
+        printf("%d", x % 10);
+    }
+    printf("\n");
+}
+
 int main() {
     int testCount;
-    printf("Enter number of test runs (10+ for better accuracy)\n");
-
+    printf("Enter number of test runs (20+ for stability verification)\n");
     scanf("%d", &testCount);
-    int *testResults = malloc(testCount * sizeof(int));
+    float *testResults = malloc(testCount * sizeof(float));
+    float *perfData = malloc(testCount * sizeof(float));
 
-    printf("Cpu Test Beginning Now!\n");
+    printf("CPU Test Beginning Now!\n");
     Sleep(1000);
 
-    for(int q = 0; q < testCount; q++){
-    
+    // Get processor group information
+    ProcessorGroupInfo* groupInfo;
+    DWORD groupCount;
+    GetProcessorGroupInfo(&groupInfo, &groupCount);
 
-    SYSTEM_INFO sysInfo;
-    GetSystemInfo(&sysInfo);
-    DWORD numCPU = sysInfo.dwNumberOfProcessors;
+    // Calculate total number of processors across all groups
+    DWORD totalProcessors = 0;
+    for (DWORD i = 0; i < groupCount; i++) {
+        totalProcessors += groupInfo[i].processorCount;
+    }
 
-    HANDLE* hThreads = (HANDLE*)malloc(numCPU * sizeof(HANDLE));
-    ThreadData* threadData = (ThreadData*)malloc(numCPU * sizeof(ThreadData));
+    HANDLE* hThreads = (HANDLE*)malloc(totalProcessors * sizeof(HANDLE));
+    ThreadData* threadData = (ThreadData*)malloc(totalProcessors * sizeof(ThreadData));
     volatile bool should_exit = false;
+    volatile LONG64 loop_counter = 0;
 
     LARGE_INTEGER frequency, start, end;
 
-    SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_HIGHEST);
+    SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_NORMAL);
     
     // Allocate and initialize arrays
-    for (DWORD i = 0; i < numCPU; i++) {
+    for (DWORD i = 0; i < totalProcessors; i++) {
         threadData[i].array = (__m256i*)_aligned_malloc(ARRAY_SIZE * sizeof(__m256i), 32);
         threadData[i].should_exit = &should_exit;
+        threadData[i].loop_counter = &loop_counter;
         for (int j = 0; j < ARRAY_SIZE; j++) {
             threadData[i].array[j] = _mm256_set1_epi32(j);
         }
     }
 
-    
+    for (int q = 0; q < testCount; q++) {
+        should_exit = false;
 
-    QueryPerformanceFrequency(&frequency);
-    QueryPerformanceCounter(&start);
+        QueryPerformanceFrequency(&frequency);
+        QueryPerformanceCounter(&start);
 
-    
-    
-        for (DWORD i = 0; i < numCPU; i++) {
-            threadData[i].thread_id = i;
-            
-            hThreads[i] = (HANDLE)_beginthreadex(NULL, 0, (unsigned int (__stdcall *)(void *))WorkerThread, &threadData[i], 0, NULL);
-            SetThreadAffinity(hThreads[i], i);
-            SetThreadPriority(hThreads[i], THREAD_PRIORITY_TIME_CRITICAL);
+        DWORD threadIndex = 0;
+        for (DWORD g = 0; g < groupCount; g++) {
+            for (DWORD i = 0; i < groupInfo[g].processorCount; i++) {
+                threadData[threadIndex].thread_id = threadIndex;
+                hThreads[threadIndex] = (HANDLE)_beginthreadex(NULL, 0, (unsigned int (__stdcall *)(void *))WorkerThread, &threadData[threadIndex], 0, NULL);
+                SetThreadAffinity(hThreads[threadIndex], groupInfo[g].groupId, i);
+                SetThreadPriority(hThreads[threadIndex], THREAD_PRIORITY_NORMAL);
+                threadIndex++;
+            }
         }
-    
-    
 
-    Sleep(TEST_DURATION_MS);
-    should_exit = true;
+        Sleep(5000);
+        perfData[q] = perf / 1000;
+        perf = 0;
+        should_exit = true;
 
-    for (DWORD i = 0; i < numCPU; i++) {
-        WaitForSingleObject(hThreads[i], INFINITE);
-        CloseHandle(hThreads[i]);
+        // Wait for all threads to finish
+        WaitForMultipleObjects(totalProcessors, hThreads, TRUE, INFINITE);
+        
+        QueryPerformanceCounter(&end);
+
+        for (DWORD i = 0; i < totalProcessors; i++) {
+            CloseHandle(hThreads[i]);
+        }
+        
+        testResults[q] = perfData[q];
+        
+        if (q > 0) {
+            printASCIILogPointGraph(testResults, q);
+            Sleep(1000);
+        }
+
+        should_exit = false;  // Reset for next run
     }
 
-    QueryPerformanceCounter(&end);
+    printf("\nTesting Complete!\n");
 
-    double elapsed = (double)(end.QuadPart - start.QuadPart) / frequency.QuadPart;
-    printf("\nRun %d: Gflops: %.2f", q, numCPU * 1000/elapsed);
-    testResults[q] = numCPU * 1000/elapsed;
-    
+    // Adjust the calculation to account for the skipped run
+    float sum = 0, sumSq = 0;
+    int effectiveCount = testCount - 1;  // We're effectively using one less run
+    for (int i = 0; i < effectiveCount; i++) {  
+        sum += testResults[i];
+        sumSq += testResults[i] * testResults[i];
+    }
 
-    for (DWORD i = 0; i < numCPU; i++) {
+    float average = sum / effectiveCount;
+    float variance = (sumSq - sum * sum / effectiveCount) / (effectiveCount - 1);
+    float stdev = sqrt(variance);
+    float stdevPercent = (stdev / average) * 100;
+
+
+    printf("\nAverage MFLOPS: %.2f\n", average);
+    printf("Standard Deviation: %.2f MFLOPS (%.2f%%)\n", stdev, stdevPercent);
+
+    if(testCount <= 19) {  // Adjusted for the skipped run
+        printf("\nStability unverified, 20 runs minimum to verify stability!");
+    } else if (stdevPercent <= 1) {
+        printf("\nCongratulations! Your cpu overclock is officially stable!");
+    } else {
+        printf("\nWomp Womp, your cpu overclock is UNSTABLE!");
+    }
+
+     // Clean up
+    free(groupInfo);
+    free(hThreads);
+    for (DWORD i = 0; i < totalProcessors; i++) {
         _aligned_free(threadData[i].array);
     }
-    free(hThreads);
     free(threadData);
-
-    }
-    printf("\n\nTesting Complete!\n");
-
-    int sum = 0;
-    for (int i = 0; i < testCount; ++i) {
-        sum += testResults[i];
-    }
-    float average = (float)sum / testCount;
-
-    printf("\n Average Gflops: %.2f\n", average);
-
-    scanf(" ");
     free(testResults);
+    free(perfData);
+
+    scanf("\n ");  // Wait for user input before closing
     return 0;
 }
